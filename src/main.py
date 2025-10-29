b29ba0a9caedeb39beea5e2bee335c60f3eee0da
Lucía Martínez <lucia.martinez@gracia.lasalle.cat>
2025-10-29 12:59:43 +0100

    Revert "Crear clase Character y CharacterFilms con heréncia"
    This reverts commit ecc4ed79fac36a3cbe557b044b7b579171e5daf7.

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import json
-from character_films import CharacterFilms
-
-def main():
-    character_film_list = []
-    print("¡Que la fuerza te acompañe!")
-
-if __name__ == "__main__":
-    main()
