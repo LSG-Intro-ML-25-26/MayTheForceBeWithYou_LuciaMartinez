ecc4ed79fac36a3cbe557b044b7b579171e5daf7
Lucía Martínez <lucia.martinez@gracia.lasalle.cat>
2025-10-29 12:53:50 +0100

    Crear clase Character y CharacterFilms con heréncia

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import json
+from character_films import CharacterFilms
+
+def main():
+    character_film_list = []
+    print("¡Que la fuerza te acompañe!")
+
+if __name__ == "__main__":
+    main()
